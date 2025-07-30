01be6d34bcd487d089ab29aca914b6a1c3395f17
Cabbage0211 <yang.leo.0211@gmail.com>
2025-07-30 14:32:26 +0800

    GBIS不再管理current_player，如有需要，自行管理

--- addons/grid_base_inventory_system/core/grid_base_inventory_system.gd
+++ addons/grid_base_inventory_system/core/grid_base_inventory_system.gd
@@ -65,8 +65,6 @@ var inventory_names: Array[String]
 ## 所有商店的name
 var shop_names: Array[String]
 
-## 当前角色，如果是单角色，不予理会即可，如果是多角色，操作每个角色前应更新这个值
-var current_player: String = DEFAULT_PLAYER
 ## 当前角色的背包，用于快捷脱装备和购买装备时物品的去向，多角色请及时更新
 var current_inventories: Array[String] = []
 ## 当前保存路径
